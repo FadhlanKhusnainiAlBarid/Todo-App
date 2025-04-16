f9b5051226987029d676e1229865ed99138362cc
aXLan666 <angaragilang266@gmail.com>
2025-04-16 21:42:34 +0700

    Create useActionTodo width sweetalert2 Finish

--- client/src/components/todo.jsx
+++ client/src/components/todo.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Checkbox } from "flowbite-react";
 import { Trash, Pencil } from "lucide-react";
 import { customThemeCheckbox } from "../assets/customTheme";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
+import useActionTodo from "../hooks/useActionTodo";
 // import { useSortable } from "@dnd-kit/react/sortable";
 
 function Todo({
@@ -15,76 +14,12 @@ function Todo({
   isError,
 }) {
   // const { ref } = useSortable({ id: data.id, index });
+  const { showSwalDelete, showSwalUpdate } = useActionTodo(
+    data,
+    handleDelete,
+    handleUpdate
+  );
   const [updateTodo, setupdateTodo] = useState(data.todo);
-
-  const showSwalDelete = () => {
-    withReactContent(Swal)
-      .fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!",
-      })
-      .then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: "Deleted!",
-            text: "Your todo has been deleted.",
-            icon: "success",
-          });
-          handleDelete(data.id);
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-          Swal.fire({
-            title: "Cancelled",
-            text: "Completed you todo before deleted :)",
-            icon: "error",
-          });
-        }
-      });
-  };
-
-  const showSwalUpdate = () => {
-    const swalStyling = withReactContent(Swal).mixin({
-      customClass: {
-        input: "",
-        inputLabel: "text-2xl",
-      },
-      inputValue: `${updateTodo}`,
-    });
-
-    swalStyling
-      .fire({
-        title: "Edit todo",
-        willOpen: () => {
-          const input = Swal.getInput();
-          input.oninput = (e) => setupdateTodo(e.target.value);
-        },
-        inputLabel: "Todo",
-        input: "text",
-        inputAttributes: {
-          autocapitalize: "off",
-        },
-        showCancelButton: true,
-        confirmButtonText: "Yes, Edit todo!",
-        allowOutsideClick: () => !Swal.isLoading(),
-      })
-      .then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: "Edit!",
-            text: "Your todo has been Updated.",
-            icon: "success",
-          });
-        }
-      });
-  };
-
-  useEffect(() => {
-    console.log(updateTodo);
-  }, [updateTodo]);
 
   return (
     <li className="flex justify-between w-full">
@@ -107,7 +42,7 @@ function Todo({
           <Trash className="w-6 h-6 md:w-7 md:h-7 xl:w-10 xl:h-10" />
         </button>
         <button
-          onClick={showSwalUpdate}
+          onClick={() => showSwalUpdate(updateTodo, setupdateTodo)}
           className="cursor-pointer text-blue-400 active:scale-95"
         >
           <Pencil className="w-6 h-6 md:w-7 md:h-7 xl:w-10 xl:h-10" />
