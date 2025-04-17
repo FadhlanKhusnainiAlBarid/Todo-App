564ff895eb861cc9b55c59a69289b7b5d07563a0
aXLan666 <angaragilang266@gmail.com>
2025-04-17 16:06:38 +0700

    Create: validation for sweetalert2 edit todo

--- client/src/components/todoCompleted.jsx
+++ client/src/components/todoCompleted.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { Checkbox } from "flowbite-react";
 import { Trash, Pencil } from "lucide-react";
-import ModalDelete from "./modalDelete";
-import ModalUpdate from "./modalUpdate";
 import { customThemeCheckbox } from "../assets/customTheme";
+import useActionTodo from "../hooks/useActionTodo";
 
 function TodoCompleted({
   data,
@@ -13,8 +12,14 @@ function TodoCompleted({
   handleUpdate,
   isError,
 }) {
-  const [modalDelete, setmodalDelete] = useState(false);
-  const [modalUpdate, setmodalUpdate] = useState(false);
+  const { showSwalDelete, showSwalUpdate } = useActionTodo(
+    data,
+    handleDelete,
+    handleUpdate,
+    isError
+  );
+  const [updateTodo, setupdateTodo] = useState(data.todo);
+
   return (
     <li className="flex justify-between w-full">
       <div className="flex items-start gap-2.5 w-3/4 md:w-[91%] xl:w-[92%] 2xl:w-[94%]">
@@ -29,31 +34,18 @@ function TodoCompleted({
       </div>
       <div className="space-x-2.5">
         <button
-          onClick={() => setmodalDelete(true)}
+          onClick={() => showSwalDelete(data.completed)}
           className="cursor-pointer text-red-400 active:scale-95"
         >
           <Trash className="w-6 h-6 md:w-7 md:h-7 xl:w-10 xl:h-10" />
         </button>
         <button
-          onClick={() => setmodalUpdate(true)}
+          onClick={() => showSwalUpdate(updateTodo, setupdateTodo)}
           className="cursor-pointer text-blue-400 active:scale-95"
         >
           <Pencil className="w-6 h-6 md:w-7 md:h-7 xl:w-10 xl:h-10" />
         </button>
       </div>
-      <ModalDelete
-        data={data}
-        modalDelete={modalDelete}
-        setmodalDelete={setmodalDelete}
-        handleDelete={handleDelete}
-      />
-      <ModalUpdate
-        data={data}
-        modalUpdate={modalUpdate}
-        setmodalUpdate={setmodalUpdate}
-        handleUpdate={handleUpdate}
-        isError={isError}
-      />
     </li>
   );
 }
